456240f452b6848c8bd1114d160df0801b2f886e
shaek666 <shaekahmed69@gmail.com>
2026-05-06 01:29:22 +0600

    Add .gitignore
    Signed-off-by: shaek666 <shaekahmed69@gmail.com>

--- obj/Debug/net10.0/another-Identity-Framework.AssemblyInfo.cs
+++ obj/Debug/net10.0/another-Identity-Framework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("another-Identity-Framework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4094f0fd8127ee37377eea07c9ad45617285ac88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+320c00e72ddd0802cf0dd64886fe26e24a818127")]
 [assembly: System.Reflection.AssemblyProductAttribute("another-Identity-Framework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("another-Identity-Framework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
