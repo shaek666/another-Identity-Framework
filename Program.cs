320c00e72ddd0802cf0dd64886fe26e24a818127
shaek666 <shaekahmed69@gmail.com>
2026-05-06 01:24:05 +0600

    Add bearer token implementation
    Signed-off-by: shaek666 <shaekahmed69@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,140 +1,70 @@
-    using System.Net;
-    using System.Security.Claims;
-    using Microsoft.AspNetCore.Authentication;
-    using Microsoft.AspNetCore.Authentication.Cookies;
-    using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Experimental;
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    // builder.Services
-    //     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    //     .AddCookie(options =>
-    //     {
-    //         options.Cookie.Name = "Friday";
+var app = builder.Build();
 
-    //         options.Events.OnRedirectToLogin = context =>
-    //         {
-    //             context.Response.StatusCode = 401;
-    //             return Task.CompletedTask;
-    //         };
-    //     });
+var key = "secret-key-123456789101112akakakakcjncnckclcnccnaocnixnicnzincizxj";
 
+app.MapGet("/secure", (HttpContext context) =>
+{
+    var authHeader = context.Request.Headers.Authorization.ToString();
+    if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer ")) return Results.Unauthorized();
 
-    // builder.Services.AddAuthorization();
-
-
-    var app = builder.Build();
-
-    app.Use(async (context, next) =>
+    var token = authHeader.Replace("Bearer ", "");
+    var handler = new JwtSecurityTokenHandler();
+    var parameters = new TokenValidationParameters
     {
-        if (context.Request.Path.StartsWithSegments("/login"))
-        {
-            await next();
-            return;
-        }
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
 
-        var authCookie = context.Request.Headers.Cookie.FirstOrDefault(c => c.StartsWith(("friday")));
+    var principal = handler.ValidateToken(token, parameters, out var validatedToken);
+    if (principal == null) return Results.Unauthorized();
 
-        if (authCookie == null || authCookie.Length <= 0)
-        {
-            context.Response.StatusCode = 401;
-            await context.Response.WriteAsync("Unauthorized: No authentication cookie found.");
-            return;
-        }
+    var userName = principal.FindFirst(ClaimTypes.Name).Value;
+    var role = principal.FindFirst(ClaimTypes.Role).Value;
 
-        var payload = authCookie.Split("=").Last();
-        var parts = payload.Split(":");
-        var key = parts[0];
-        var value = parts[1];
-
-        var claims = new List<Claim>
-        {
-            new(key, value)
-        };
-
-        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-        context.User = new ClaimsPrincipal(claimsIdentity);
-        await next();
+    return Results.Ok(new
+    {
+        message = "Valid token",
+        userName,
+        role
     });
+});
 
-    // app.UseAuthentication();
-    // app.UseAuthorization();
-
-    // app.MapGet("/cookie-authorized", (HttpContext context) =>
-    // {
-    //     return Results.Ok("You're authenticated by Cookie"); // This ain't an authenticated route, like 
-    // }).RequireAuthorization();
-
-    // app.MapGet("/login", async (string userName, string password, HttpContext context) =>
-    // {
-    //     if (userName != "jackiechan" && password != "password") return Results.Unauthorized();
-
-    //     //Generating secrets if userName == jackiechan and password is password, assuming these will always be the username and password as we are hardcoding it.
-    //     var claims = new List<Claim>
-    //     {
-    //         new("username", userName),
-    //         new("movie", "rush-hour-3")
-    //     };
-    //     var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-    //     var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-
-    //     await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
-    //     return Results.Ok();
-    // });
-
-    // app.MapGet("/logout", (string userName, string password, HttpContext context) =>
-    // {
-    //     if (userName != "jackiechan" && password != "password") return Results.Unauthorized();
-
-    //     var secret = $"username:{userName}";
-    //     context.Response.Headers["set-cookie"] = $"sunday= {secret}";
-    //     return Results.Ok();
-    // });
-
-    app.MapGet("/cookie-authorized", (HttpContext context) =>
+app.MapGet("/login-with-jwt", (string userName, string password) =>
+{
+    if (userName != "jackiechan" || password != "password") return Results.Unauthorized();
+    var claims = new List<Claim>
     {
-        var value = context.User.FindFirst("username");
-        return Results.Ok(value?.Value);
-    });
+        new(ClaimTypes.Name, userName),
+        new(ClaimTypes.Role, "admin")
+    };
 
-    app.MapGet("/login", async (string userName, string password, HttpContext context) =>
+    var tokenDescriptor = new SecurityTokenDescriptor()
     {
-        if (userName != "jackiechan" || password != "password") return Results.Unauthorized();
-        var secret = $"username:{userName}";
-        context.Response.Headers["set-cookie"] = $"friday={secret}";
-        return Results.Ok("Login successful! Cookie has been set.");
-    });
+        Subject =  new ClaimsIdentity(claims),
+        Expires =  DateTime.UtcNow.AddMinutes(30),
+        SigningCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+            SecurityAlgorithms.HmacSha256Signature
+        )
+    };
 
-    app.Run();
+    var handler = new JwtSecurityTokenHandler();
+    var token = handler.CreateToken(tokenDescriptor);
+    var jwt = handler.WriteToken(token);
 
-    public class AuthFactory
-    {
-        public static async Task SignInAsync(string scheme)
-        {
-            if (scheme == "cookie") await new CookieAuthService().SignInAsync();
-            if (scheme == "bearer") await new BearerAuthService().SignInAsync();
-        }
-    }
+    return Results.Ok(new { token = jwt });
+});
 
-    public interface IAuthService
-    {
-        public Task SignInAsync();
-    }
-
-    public class CookieAuthService : IAuthService
-    {
-        public Task SignInAsync()
-        {
-            Console.WriteLine("Executing Strategy: Sign in with Cookie");
-            return Task.CompletedTask;
-        }
-    }
-
-    public class BearerAuthService : IAuthService
-    {
-        public Task SignInAsync()
-        {
-            Console.WriteLine("Executing Strategy: Sign in with Bearer Token");
-            return Task.CompletedTask;
-        }
-    }
+app.Run();
